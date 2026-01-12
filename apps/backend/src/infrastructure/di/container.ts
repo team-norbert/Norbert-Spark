41fb1afe14a25dba5b9e6d8c66916e0ad2c57203
TCotton <me@andywalpole.me>
2026-01-12 17:23:24 +0000

    feat: created ExternalCloudflareService class and ExternalCloudflareServicePort port
    # Conflicts:
    #	apps/backend/src/infrastructure/di/container.ts

--- apps/backend/src/infrastructure/di/container.ts
+++ apps/backend/src/infrastructure/di/container.ts
@@ -94,7 +94,7 @@ export class Container {
   public readonly aiExtractDataController: AIExtractDataController
 
   // Audit log
-  public readonly auditLog: AuditLogPort
+  public readonly auditLog: AuditLogRepository
 
   /**
    * Private constructor to enforce Singleton pattern
