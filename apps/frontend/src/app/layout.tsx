ed456635df50f1ff28ce3297d6e5af005dfd2a07
TCotton <me@andywalpole.me>
2026-02-16 16:18:52 +0000

    style: added Roboto Mono font to the NextJS app

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -20,6 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <SessionProvider>
           <QueryProvider>
